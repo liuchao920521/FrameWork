17ff8f0e3614195e3854a0393b7c61fe987b6a1f
LiuChao <563487767@qq.com>
2020-07-09 15:32:55 +0800

    Add files via upload

---
+++ Assets/Res/Script/CSharp/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace WZQ
+{
+	public class GameManager : MonoBehaviour
+	{
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			Init();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		void Init()
+		{
+			Debug.Log("***********初始化***************");
+			DontDestroyOnLoad(gameObject);  //防止销毁自己		
+
+		}
+
+		public void BloodBar()
+		{
+
+		}
+
+	}
+}
---
+++ Assets/Res/Script/CSharp/LuaManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LuaInterface;
+using System.IO;
+
+namespace WZQ
+{
+	public class LuaManager : MonoBehaviour
+	{
+		private LuaState lua;
+		//private LuaLoader loader;
+		private LuaLooper loop = null;
+		void Awake()
+		{
+			lua = new LuaState();
+			lua.LuaSetTop(0);
+			LuaBinder.Bind(lua);
+			DelegateFactory.Init();
+			LuaCoroutine.Register(lua, this);
+		}
+	}
+}
